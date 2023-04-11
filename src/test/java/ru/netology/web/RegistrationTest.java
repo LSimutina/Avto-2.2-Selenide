a8dd19ba151fc3e36fcc250c1bcdc74c10389526
Lidiya <simutina.1994@mail.ru>
2023-04-11 11:31:09 +0300

    add Test

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -1,5 +1,6 @@
 package ru.netology.web;
 
+import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
 
@@ -7,7 +8,6 @@ import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 
-import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Selenide.*;
 
@@ -22,13 +22,18 @@ class RegistrationTest {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Казань");
-        $("[data-test-id=date] input.input__control").doubleClick().sendKeys("BACKSPACE");
+//        $("[data-test-id=date] input.input__control").doubleClick().sendKeys("BACKSPACE");
+        $("[data-test-id=date] input.input__control").doubleClick().sendKeys("DELETE");
         String dateOfMeeting = date(6, "dd.MM.yyyy");
         $("[data-test-id=date] input.input__control").sendKeys(dateOfMeeting);
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+71234567890");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-        $x("//div[contains(text(), 'Успешно!')]").shouldBe(visible, Duration.ofSeconds(15));
+//        $x("//div[contains(text(), 'Успешно!')]").shouldBe(visible, Duration.ofSeconds(15));
+
+        $(".notification__content")
+                .shouldHave(Condition.text("Встреча успешно забронирована на " + dateOfMeeting), Duration.ofSeconds(15))
+                .shouldBe(Condition.visible);
     }
 }
