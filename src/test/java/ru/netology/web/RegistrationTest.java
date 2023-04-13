0e96c7cdc50493344a294621850daf4db905faa2
Lidiya <simutina.1994@mail.ru>
2023-04-13 14:18:13 +0300

    add completion

--- src/test/java/ru/netology/web/RegistrationTest.java
+++ src/test/java/ru/netology/web/RegistrationTest.java
@@ -3,6 +3,7 @@ package ru.netology.web;
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
@@ -22,16 +23,13 @@ class RegistrationTest {
         Configuration.holdBrowserOpen = true;
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Казань");
-//        $("[data-test-id=date] input.input__control").doubleClick().sendKeys("BACKSPACE");
-        $("[data-test-id=date] input.input__control").doubleClick().sendKeys("DELETE");
-        String dateOfMeeting = date(6, "dd.MM.yyyy");
+        $("[data-test-id=date] input.input__control").doubleClick().sendKeys(Keys.BACK_SPACE);
+        String dateOfMeeting = date(3, "dd.MM.yyyy");
         $("[data-test-id=date] input.input__control").sendKeys(dateOfMeeting);
         $("[data-test-id=name] input").setValue("Иванов Иван");
         $("[data-test-id=phone] input").setValue("+71234567890");
         $("[data-test-id='agreement']").click();
         $$("button").find(exactText("Забронировать")).click();
-//        $x("//div[contains(text(), 'Успешно!')]").shouldBe(visible, Duration.ofSeconds(15));
-
         $(".notification__content")
                 .shouldHave(Condition.text("Встреча успешно забронирована на " + dateOfMeeting), Duration.ofSeconds(15))
                 .shouldBe(Condition.visible);
